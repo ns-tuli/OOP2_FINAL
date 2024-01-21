dcb37a2dd32bbe51dd7a189227b305e0dab44f59
Nusrat Siddique <113282774+ns-tuli@users.noreply.github.com>
2024-01-21 19:11:25 +0600

    Update ShopSalesReport.cs

--- ShopSalesReport.cs
+++ ShopSalesReport.cs
@@ -1,4 +1,17 @@
-﻿using
+using OOP2_FINAL_PROJECT;
+using System;
+
+
+class ShopSalesReport
+{
+    public void ViewShopDetails(Shop shop)
+    {
+        Console.WriteLine($"Shop Details: {shop.ShopName}, Code: {shop.ShopCode}, Type: {shop.ShopType}, Address: {shop.ShopAddress}");
+    }
+}
+
+
+
 
 
 
